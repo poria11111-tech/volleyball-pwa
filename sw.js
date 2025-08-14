142c6ccfb0ed0e4f01ae9c444db5e5978c9db771
poria11111-tech <poria11111@gmail.com>
2025-08-14 13:29:02 +0330

    Add files via upload
    update

---
+++ sw.js
@@ -0,0 +1,41 @@
+
+const CACHE_NAME = 'vb-pwa-v1';
+const OFFLINE_URLS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(req).then(cached => {
+      const fetchPromise = fetch(req).then(networkRes => {
+        if (networkRes && networkRes.status === 200 && networkRes.type === 'basic') {
+          const clone = networkRes.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(req, clone));
+        }
+        return networkRes;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
