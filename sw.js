8b1cc3ee53e906402bff97a3a04eb8f56754b8f8
poria11111-tech <poria11111@gmail.com>
2025-08-17 18:49:24 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "volleyball-cache-v1";
+const urlsToCache = [
+  "/test6.html",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
